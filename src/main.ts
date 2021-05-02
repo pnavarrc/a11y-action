15cb2d2dcb674175b45a9ca9c7b2b21e2e60e6be
Pablo Navarro Castillo <pnavarrc@gmail.com>
2021-05-01 23:01:17 -0700

    generate report

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,36 @@
-import * as core from '@actions/core'
+// import * as core from '@actions/core'
+import {AxePuppeteer} from '@axe-core/puppeteer'
+import {writeFileSync} from 'fs'
+import puppeteer from 'puppeteer'
 
-async function run(): Promise<void> {
-  try {
-    const url: string = core.getInput('url')
-    core.debug(`Checking ${url}`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+async function check(): Promise<void> {
+  const browser = await puppeteer.launch()
+  const page = await browser.newPage()
+  await page.setBypassCSP(true)
 
-    core.debug(new Date().toTimeString())
-    core.debug(`url: ${url}`)
-    core.debug(new Date().toTimeString())
+  await page.goto('https://www.covidactnow.org')
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+  const results = await new AxePuppeteer(page).analyze()
+  writeFileSync('demo.json', JSON.stringify(results, null, 2))
+  // console.log(results)
+
+  await page.close()
+  await browser.close()
 }
 
-run()
+// async function run(): Promise<void> {
+//   try {
+//     const url: string = core.getInput('url')
+//     core.debug(`Checking ${url}`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+
+//     core.debug(new Date().toTimeString())
+//     core.debug(`url: ${url}`)
+//     core.debug(new Date().toTimeString())
+
+//     core.setOutput('time', new Date().toTimeString())
+//   } catch (error) {
+//     core.setFailed(error.message)
+//   }
+// }
+
+check()
