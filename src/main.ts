c34685438674ed404bde7620cdef9f38582f8fc1
Pablo Navarro Castillo <pnavarrc@gmail.com>
2021-05-01 17:30:29 -0700

    demo action setup

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const url: string = core.getInput('url')
+    core.debug(`Checking ${url}`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
 
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
+    core.debug(`url: ${url}`)
     core.debug(new Date().toTimeString())
 
     core.setOutput('time', new Date().toTimeString())
